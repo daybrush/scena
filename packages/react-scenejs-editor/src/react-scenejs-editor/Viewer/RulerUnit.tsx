f2afd57b9fceef90fb217a889b958ded13ef93cd
Daybrush <daybrush@gmail.com>
2019-10-05 18:22:03 +0900

    fix: Fix RulerUnit

--- packages/react-scenejs-editor/src/react-scenejs-editor/Viewer/RulerUnit.tsx
+++ packages/react-scenejs-editor/src/react-scenejs-editor/Viewer/RulerUnit.tsx
@@ -3,12 +3,11 @@ import { prefix } from "../utils";
 import styled from "react-css-styler";
 import { prefixCSS } from "framework-utils";
 import { IObject } from "@daybrush/utils";
-import RulerDivisionElement from "./RulerDivision";
 
 const divisions: JSX.Element[] = [];
 
 for (let i = 0; i < 10; ++i) {
-    divisions.push(<RulerDivisionElement className={prefix("ruler-division")} key={i} />);
+    divisions.push(<div className={prefix("ruler-division")} key={i} />);
 }
 const RulerUnitElement = styled("div", prefixCSS("scenejs-editor-", `
 {
@@ -38,6 +37,50 @@ const RulerUnitElement = styled("div", prefixCSS("scenejs-editor-", `
     top: calc(100% - 4px);
     left: 2px;
     transform: rotate(-90deg);
+}
+
+.ruler-division {
+    position: relative;
+}
+.ruler-division:before {
+    content: "";
+    position: absolute;
+    background: #777;
+    bottom: 0;
+}
+.horizontal :host .ruler-division {
+    display: inline-block;
+    width: 10%;
+    height: 100%;
+}
+.vertical :host .ruler-division {
+    display: block;
+    width: 100%;
+    height: 10%;
+}
+.horizontal :host .ruler-division:before {
+    width: 1px;
+    height: 10px;
+}
+.vertical :host .ruler-division:before {
+    width: 10px;
+    height: 1px;
+    bottom: 0;
+    right: 0;
+}
+.horizontal :host .ruler-division:nth-child(2n):before {
+    height: 7px;
+}
+.vertical :host .ruler-division:nth-child(2n + 1):before {
+    width: 7px;
+}
+.horizontal :host .ruler-division:first-child:before {
+    width: 1px;
+    height: 100%;
+}
+.vertical :host .ruler-division:last-child:before {
+    height: 1px;
+    width: 100%;
 }
 `));
 const RulerUnit = (props: { px: number, style: IObject<any> }) => (
