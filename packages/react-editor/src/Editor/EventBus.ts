3be2f6e9bc0cb5d93033177b075345ad630c4f1f
daybrush <daybrush@gmail.com>
2020-06-27 14:50:56 +0900

    feat: add EventBus

--- packages/react-editor/src/Editor/EventBus.ts
+++ packages/react-editor/src/Editor/EventBus.ts
@@ -1,5 +1,18 @@
 import Component from "@egjs/component";
+import { IObject } from "@daybrush/utils";
+import { request } from "http";
 
-const EventBus = new Component();
+class Bus extends Component {
+    private eventMap: IObject<number> = {};
+    requestTrigger(name: string, params: IObject<any>) {
+        const eventMap = this.eventMap;
+        cancelAnimationFrame(eventMap[name] || 0);
+
+        eventMap[name] = requestAnimationFrame(() => {
+            this.trigger(name, params);
+        });
+    }
+}
+const EventBus = new Bus();
 
 export default EventBus;
