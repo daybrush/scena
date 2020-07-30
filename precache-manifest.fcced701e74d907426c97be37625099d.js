e5e422c70fefeabd77733f449b1081993ed15773
daybrush <daybrush@gmail.com>
2020-07-30 11:41:56 +0900

    Updates

---
+++ precache-manifest.fcced701e74d907426c97be37625099d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a18d75403897b14df9a068b611821e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16118948d06f56d66e82",
+    "url": "./static/css/main.b262ec7b.chunk.css"
+  },
+  {
+    "revision": "b49d1555a28a8891b0f7",
+    "url": "./static/js/2.11befda8.chunk.js"
+  },
+  {
+    "revision": "c72e1c41e982643e494a10430fe98013",
+    "url": "./static/js/2.11befda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16118948d06f56d66e82",
+    "url": "./static/js/main.e7ba04f5.chunk.js"
+  },
+  {
+    "revision": "f160b7ef8aa72a56bfee",
+    "url": "./static/js/runtime-main.be3849bb.js"
+  }
+]);
